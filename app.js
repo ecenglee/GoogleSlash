781e0825fd219bcc428fa016d23e50d5fbd38061
beezebrown <m.brown298@gmail.com>
2016-07-09 16:56:50 -0700

    test

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000);
-console.log('Server running at localhost:3000.');
+// app.listen(3000);
+// console.log('Server running at localhost:3000.');
 
 module.exports = app;
